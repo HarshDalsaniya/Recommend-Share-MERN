a7c5004664a47466c5eb5515fee78a127aa6fa01
maulik_dtl <maulikm@droptechnolab.com>
2021-07-27 17:35:03 +0530

    navbar changes

--- next-recommend-share/components/NavBar/NavBar.js
+++ next-recommend-share/components/NavBar/NavBar.js
@@ -38,111 +38,66 @@ export const NavBar = (props) => {
                                     <span />
                                 </div>
                             </button>
-                            <div className="collapse navbar-collapse">
-                                <ui className="navbar-nav ms-auto mb-2 mb-lg-0 navbar-item-bg">
-                                    <li className="nav-item">
-                                        <a
-                                            className="nav-link "
-                                            href="/home-improvements-and-maintenance.html"
-                                        >
-                                            Home Improvements &amp; Maintenance
-                                        </a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link " href="/champions.html">
-                                            Champions
-                                        </a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a
-                                            className="nav-link "
-                                            href="/about-us.html"
-                                            tabIndex={-1}
-                                            aria-disabled="true"
-                                        >
-                                            About
-                                        </a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a
-                                            className="nav-link "
-                                            href="/faq.html"
-                                            tabIndex={-1}
-                                            aria-disabled="true"
-                                        >
-                                            FAQs
-                                        </a>
-                                    </li>
-                                </ui>
-                                <div className="login">
-                                    <a className="nav-link" href="/login">
-                                        Login
-                                    </a>
-                                </div>
-                                <div className="login-uesr" id="login-uesr-block-2">
-                                    <ul className="navbar-nav me-auto"></ul>
-                                </div>
-                                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0 navbar-item-bg">
-                                        <li className="nav-item">
-                                            <a
-                                                className="nav-link "
-                                                href="/home-improvements-and-maintenance.html"
-                                            >
-                                                Home Improvements &amp; Maintenance
-                                            </a>
-                                        </li>
-                                        <li className="nav-item">
-                                            <a className="nav-link " href="/champions.html">
-                                                Champions
-                                            </a>
-                                        </li>
-                                        <li className="nav-item">
-                                            <a
-                                                className="nav-link "
-                                                href="/about-us.html"
-                                                tabIndex={-1}
-                                                aria-disabled="true"
-                                            >
-                                                About
-                                            </a>
-                                        </li>
-                                        <li className="nav-item">
-                                            <a
-                                                className="nav-link "
-                                                href="/faq.html"
-                                                tabIndex={-1}
-                                                aria-disabled="true"
-                                            >
-                                                FAQs
-                                            </a>
-                                        </li>
-                                    </ul>
-                                    <div className="login">
-                                        <Link href="/login">
-                                            <a className="nav-link"> Login </a>
-                                        </Link>
-                                    </div>
-
-                                    <div className="login-uesr" id="login-uesr-block-2">
-                                        <ul className="navbar-nav me-auto"></ul>
-                                    </div>
-                                </div>
+                            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav ms-auto mb-2 mb-lg-0 navbar-item-bg">
+                                <li className="nav-item">
+                                <a
+                                    className="nav-link "
+                                    href="/home-improvements-and-maintenance.html"
+                                >
+                                    Home Improvements &amp; Maintenance
+                                </a>
+                                </li>
+                                <li className="nav-item">
+                                <a className="nav-link " href="/champions.html">
+                                    Champions
+                                </a>
+                                </li>
+                                <li className="nav-item">
+                                <a
+                                    className="nav-link "
+                                    href="/about-us.html"
+                                    tabIndex={-1}
+                                    aria-disabled="true"
+                                >
+                                    About
+                                </a>
+                                </li>
+                                <li className="nav-item">
+                                <a
+                                    className="nav-link "
+                                    href="/faq.html"
+                                    tabIndex={-1}
+                                    aria-disabled="true"
+                                >
+                                    FAQs
+                                </a>
+                                </li>
+                            </ul>
+                            <div className="login">
+                                <Link href="/login">                   
+                                <a className="nav-link"> Login </a>
+                               </Link>
                             </div>
+
+                            <div className="login-uesr" id="login-uesr-block-2">
+                                <ul className="navbar-nav me-auto"></ul>
+                            </div>
+                        </div>
                         </div>
                     </div>
                 </Container>
             </div>
         </div>
-                )
+    )
 }
 
 const mapStateToProps = (state) => ({
 
-                })
+})
 
-                const mapDispatchToProps = {
+const mapDispatchToProps = {
 
-                }
+}
 
-                export default connect(mapStateToProps, mapDispatchToProps)(NavBar)
+export default connect(mapStateToProps, mapDispatchToProps)(NavBar)
