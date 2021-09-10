bcdf2b806273a99e787551b8b466e7ef14ebf7e0
maulik_dtl <maulikm@droptechnolab.com>
2021-09-10 22:25:04 +0530

    no message

--- next-recommend-share/helper/userAuthRedirect.js
+++ next-recommend-share/helper/userAuthRedirect.js
@@ -5,15 +5,15 @@ const isAuthenticate = async(path) =>{
         const userVerify= await JSON.parse(localStorage.getItem("Recommend_Share_current_user"))
         const urls=["/","/reset-password","/login", "/register", "/home-improvements-and-maintenance", "/champions", "/about-us", "/faq", "/terms-of-use", "/gdpr", "/contact-us"];
         if(typeof userVerify=="object" && userVerify==null && !urls.includes(path)){
-            // Router.push('/login')
-            return false
+            Router.push('/login')
+            // return false
         }
         if(typeof userVerify=="object" && userVerify!=null && (path=="/login" || path=="/register" || path=="/")){
-            // Router.push("/secure")
-            return true
+            Router.push("/secure")
+            // return true
         }   
     }catch(e){
-        return false
+        // return false
         // console.log(e)
     }
 }
